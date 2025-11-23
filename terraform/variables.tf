71992dd674672cfd79691772dc22bff21498948d
ArtiomKrits <artiomk@appdome.com>
2025-11-23 22:25:34 +0200

    Add Terraform variables and example config

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+  default     = "cluster-key"
+}
+
